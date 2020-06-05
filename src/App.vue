05c48639b15e485b2379f4c1d97e3affb04efbf7
kasprzyk <piotkas1@gmail.com>
2020-06-05 15:45:53 +0200

    Login page and router added

--- src/App.vue
+++ src/App.vue
@@ -7,30 +7,7 @@
       <v-btn text rounded>Login</v-btn>
     </v-app-bar>
     <v-content>
-      <!-- Login Module -->
-      <v-card width="400" class="mx-auto mt-5">
-        <v-card-title>
-          <h1 class="display-1">Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field label="Username" prepend-icon="mdi-account-circle" />
-            <v-text-field
-              :type="showPassword ? 'text' : 'password'"
-              label="Password"
-              prepend-icon="mdi-lock"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-            />
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-      </v-card>
+      <Login />
     </v-content>
     <v-footer color="primary lighten-1" padless>
       <v-layout justify-center wrap>
@@ -53,11 +30,14 @@
 </template>
 
 <script>
+import Login from './views/Login';
 export default {
+  components: {
+    Login,
+  },
   name: 'App',
   data() {
     return {
-      showPassword: false,
       links: ['Home', 'Login'],
     };
   },
