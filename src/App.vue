e1fc2f4c6c2c216081e9a0d6d27934156d442648
kasprzyk <piotkas1@gmail.com>
2020-06-05 12:10:19 +0200

    Prepend icons, buttons

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,21 @@
       </v-card-title>
       <v-card-text>
         <v-form>
-          <v-text-field label="Username"></v-text-field>
+          <v-text-field
+            label="Username"
+            prepend-icon="mdi-account-circle"
+          ></v-text-field>
+          <v-text-field
+            type="password"
+            label="Password"
+            prepend-icon="mdi-lock"
+          ></v-text-field>
         </v-form>
       </v-card-text>
+      <v-card-actions>
+        <v-btn>Register</v-btn>
+        <v-btn>Login</v-btn>
+      </v-card-actions>
     </v-card>
   </v-app>
 </template>
