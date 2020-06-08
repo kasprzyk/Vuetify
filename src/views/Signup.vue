8ef5acb2fa846096fa988687df04c492146e9030
kasprzyk <piotkas1@gmail.com>
2020-06-08 14:56:35 +0200

    validateForm form validation

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -23,8 +23,16 @@
             :rules="agreeToTermsRules"
             required
           ></v-checkbox>
-          <v-btn type="submit" color="primary" class="mr-4">
+          <v-btn
+            type="submit"
+            color="primary"
+            class="mr-4"
+            :disabled="!formValidity"
+          >
             Submit
+          </v-btn>
+          <v-btn color="success" class="mr-4" @click="validateForm">
+            Validate Form
           </v-btn>
           <v-btn color="warning" class="mr-4" @click="resetValidation">
             Reset Validation
@@ -70,6 +78,9 @@ export default {
     },
     resetValidation() {
       this.$refs.signUpForm.resetValidation();
+    },
+    validateForm() {
+      this.$refs.signUpForm.validate();
     },
   },
 };
