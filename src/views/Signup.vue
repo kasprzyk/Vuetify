1a466b20a84ab204f311c5940d36f49f7d957cc7
kasprzyk <piotkas1@gmail.com>
2020-06-08 14:11:51 +0200

    rules

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -4,7 +4,12 @@
       <v-col>
         <h1>Signup Page</h1>
         <v-form>
-          <v-text-field label="Email" type="email" />
+          <v-text-field
+            label="Email"
+            type="email"
+            v-model="email"
+            :rules="emailRules"
+          ></v-text-field>
           <v-autocomplete
             label="Which browser do you use?"
             :items="browsers"
@@ -12,7 +17,12 @@
           <v-file-input label="Attach profile picture"></v-file-input>
           <v-text-field label="Birthday" v-model="birthday" readonly />
           <v-date-picker v-model="birthday" />
-          <v-checkbox label="Agree to terms & conditions" />
+          <v-checkbox
+            label="Agree to terms & conditions"
+            v-model="agreeToTerms"
+            :rules="agreeToTermsRules"
+            required
+          ></v-checkbox>
           <v-btn type="submit" color="primary">Submit</v-btn>
         </v-form>
       </v-col>
@@ -26,6 +36,25 @@ export default {
     return {
       birthday: '',
       browsers: ['Chrome', 'Firefox', 'Safari', 'Edge', 'Brave'],
+      agreeToTerms: false,
+      agreeToTermsRules: [
+        (value) =>
+          !!value ||
+          'You must agree to the terms and conditions to sign up for an account.',
+      ],
+      email: '',
+      emailRules: [
+        (value) => value.indexOf('@') !== 0 || 'Email should have a username.',
+        (value) => value.includes('@') || 'Email should include an @ symbol.',
+        (value) =>
+          value.indexOf('.') - value.indexOf('@') > 1 ||
+          'Email should contain a valid domain.',
+        (value) =>
+          value.includes('.') || 'Email should include a period symbol.',
+        (value) =>
+          value.indexOf('.') <= value.length - 3 ||
+          'Email should contain a valid domain extension.',
+      ],
     };
   },
 };
